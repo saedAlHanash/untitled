e8feec785ce05b22a9ff7b628a83c06d99ee773d
saed <samersame404@gmail.com>
2024-04-18 14:42:47 +0300

    fix pod and wakelock_plus

--- pod_player/wakelock_plus/test/wakelock_plus_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  // There are no unit tests here as the API in this package only forwards the
-  // calls to the platform interface (unit tested) → platform implementations.
-  // Instead, this is all tested via e2e tests in the integration tests in the
-  // example app.
-  test('no unit tests', () {});
-}
