854edd38ca5e6a968eb3228e3bc1b947f42ab4b0
saed <samersame404@gmail.com>
2024-05-03 21:41:18 +0300

    fix pod and wakelock_plus

--- lib/features/trainer/ui/widget/trainer_header.dart
+++ lib/features/trainer/ui/widget/trainer_header.dart
@@ -50,121 +50,118 @@ class TrainerHeader extends StatelessWidget {
           ),
           10.0.horizontalSpace,
           Expanded(
-            child: SizedBox(
-              height: 160.0.h,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  DrawableText(
-                    text: (trainer.name).toUpperCase(),
-                    size: 16.0.sp,
-                    fontFamily: FontFamily.robotoBold.name,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                DrawableText(
+                  text: (trainer.name).toUpperCase(),
+                  size: 16.0.sp,
+                  fontFamily: FontFamily.robotoBold.name,
+                ),
+                15.0.verticalSpace,
+                RatingBarIndicator(
+                  itemCount: 5,
+                  rating: trainer.rating.toDouble(),
+                  itemSize: 30.0.r,
+                  itemBuilder: (context, _) => Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                    size: 25.0.r,
                   ),
-                  15.0.verticalSpace,
-                  RatingBarIndicator(
-                    itemCount: 5,
-                    rating: trainer.rating.toDouble(),
-                    itemSize: 30.0.r,
-                    itemBuilder: (context, _) => Icon(
-                      Icons.star,
-                      color: Colors.amber,
-                      size: 25.0.r,
-                    ),
-                  ),
-                  15.0.verticalSpace,
-                  BioCheckWidget(
-                      title: S.of(context).privateHours,
-                      value: '+${trainer.numberOfPrivateHours}'),
-                  BlocBuilder<BookedAppointmentsCubit, BookedAppointmentsInitial>(
-                    builder: (context, state) {
-                      if (state.statuses.loading) {
-                        return MyStyle.loadingWidget();
-                      }
+                ),
+                15.0.verticalSpace,
+                BioCheckWidget(
+                    title: S.of(context).privateHours,
+                    value: '+${trainer.numberOfPrivateHours}'),
+                BlocBuilder<BookedAppointmentsCubit, BookedAppointmentsInitial>(
+                  builder: (context, state) {
+                    if (state.statuses.loading) {
+                      return MyStyle.loadingWidget();
+                    }
 
-                      final pair = state.getSession();
+                    final pair = state.getSession();
 
-                      if (pair.second == PrivetSessionState.noEver &&
-                          !AppControl.isAppleAccount) {
-                        return 0.0.verticalSpace;
-                      }
-                      return Column(
-                        children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                flex: 3,
-                                child: MyButtonRound(
-                                  color: AppColorManager.mainColor,
-                                  child: DrawableText(
-                                    text: S.of(context).chat,
-                                    drawablePadding: 3.0.w,
-                                    drawableEnd: ImageMultiType(
-                                      url: Assets.imagesChatSVG,
-                                      height: 15.0.w,
-                                      width: 15.0.w,
-                                      color: Colors.white,
-                                    ),
-                                    size: 14.0.sp,
+                    if (pair.second == PrivetSessionState.noEver &&
+                        !AppControl.isAppleAccount) {
+                      return 0.0.verticalSpace;
+                    }
+                    return Column(
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              flex: 3,
+                              child: MyButtonRound(
+                                color: AppColorManager.mainColor,
+                                child: DrawableText(
+                                  text: S.of(context).chat,
+                                  drawablePadding: 3.0.w,
+                                  drawableEnd: ImageMultiType(
+                                    url: Assets.imagesChatSVG,
+                                    height: 15.0.w,
+                                    width: 15.0.w,
                                     color: Colors.white,
                                   ),
-                                  onTap: () async {
-                                    final openRoom = await context
-                                        .read<RoomsCubit>()
-                                        .getRoomByUser(trainer.id.toString());
-
-                                    if (!context.mounted || openRoom == null) return;
-
-                                    openRoomFunction(context, openRoom);
-                                    return;
-                                  },
+                                  size: 14.0.sp,
+                                  color: Colors.white,
                                 ),
+                                onTap: () async {
+                                  final openRoom = await context
+                                      .read<RoomsCubit>()
+                                      .getRoomByUser(trainer.id.toString());
+
+                                  if (!context.mounted || openRoom == null) return;
+
+                                  openRoomFunction(context, openRoom);
+                                  return;
+                                },
                               ),
-                              15.0.horizontalSpace,
-                              Expanded(
-                                flex: 4,
-                                child: MyButtonRound(
-                                  color: AppColorManager.mainColorLight,
-                                  enable: pair.second == PrivetSessionState.active,
-                                  child: DrawableText(
-                                    text: S.of(context).videoCall,
-                                    color: Colors.white,
-                                    size: 14.0.sp,
-                                    drawablePadding: 3.0.w,
-                                    drawableEnd: ImageMultiType(
-                                      url: Assets.imagesVideo,
-                                      height: 15.0.w,
-                                      width: 15.0.w,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (_) {
-                                          return Video1(appointment: pair.first!);
-                                        },
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                          if (pair.second == PrivetSessionState.waiting)
-                            DrawableText(
-                              padding: const EdgeInsets.symmetric(vertical: 5.0).h,
-                              text:
-                                  '${S.of(context).nextSession}: ${pair.first?.startTime.formatDateTime}',
-                              size: 12.0.sp,
                             ),
-                        ],
-                      );
-                    },
-                  ),
-                ],
-              ),
+                            15.0.horizontalSpace,
+                            Expanded(
+                              flex: 4,
+                              child: MyButtonRound(
+                                color: AppColorManager.mainColorLight,
+                                enable: pair.second == PrivetSessionState.active,
+                                child: DrawableText(
+                                  text: S.of(context).videoCall,
+                                  color: Colors.white,
+                                  size: 14.0.sp,
+                                  drawablePadding: 3.0.w,
+                                  drawableEnd: ImageMultiType(
+                                    url: Assets.imagesVideo,
+                                    height: 15.0.w,
+                                    width: 15.0.w,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (_) {
+                                        return Video1(appointment: pair.first!);
+                                      },
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        if (pair.second == PrivetSessionState.waiting)
+                          DrawableText(
+                            padding: const EdgeInsets.symmetric(vertical: 5.0).h,
+                            text:
+                                '${S.of(context).nextSession}: ${pair.first?.startTime.formatDateTime}',
+                            size: 12.0.sp,
+                          ),
+                      ],
+                    );
+                  },
+                ),
+              ],
             ),
           ),
         ],
